030395dc9d42d68a7e1b5b5cffcab139b62aeb6a
Arseniy Sitnikov <74531830+arscoolik@users.noreply.github.com>
2022-09-20 18:05:37 +0300

    Update oleg_algo.py

--- RandomCode/oleg_algo.py
+++ RandomCode/oleg_algo.py
@@ -1,11 +1,10 @@
-if __name__ == "__main__":
-    n = int(input())
-    lst = list(map(int, input().split()))
-    length = len(lst)
-    lst = sum(lst)
-    ans = 0
-    cur = 1
-    for i in range(1, length):
-        ans += cur
-        cur *= (length - i)/i
-    print(int((ans + 1) * lst))
+n = int(input())
+lst = list(map(int, input().split()))
+length = len(lst)
+lst = sum(lst)
+ans = 0
+cur = 1
+for i in range(1, length):
+    ans += cur
+    cur *= (length - i)/i
+print(int((ans + 1) * lst))
